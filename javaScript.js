b1951e589e274468cac1a2d789ff7c0b3fb465f7
Molepo21 <kgamakigift@gmail.com>
2023-06-02 17:13:29 +0200

    all changes

---
+++ javaScript.js
@@ -0,0 +1,4 @@
+document.getElementById("button").onclick = function(){
+    document.getElementById("confirm").innerHTML = "check your email for confirmation";
+    // document.getElementById("button").style.display = "none";
+}
